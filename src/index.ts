b68ae2852f4c017ee77a8ae44b8a519fcaa9c129
Felipe Araújo <fplopesaraujo@gmail.com>
2022-09-20 09:42:29 -0300

    Lint

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import express from 'express'
 const app = express()
 
 app.listen(process.env.PORT, () => {
-	console.log(`- App running at http://localhost:${process.env.PORT}`)
+	console.log(`[INFO] App running at http://localhost:${process.env.PORT}`)
 })
 
 app.get('/', (req, res) => {
