6741f5f75ba3c23cb322fe6d1ef18ff3077ccd15
Felipe Araújo <fplopesaraujo@gmail.com>
2022-09-25 01:48:11 -0300

    Fix: endpoint url

--- src/index.ts
+++ src/index.ts
@@ -7,3 +7,4 @@ function startApp() {
 }
 
 startApp();
+
--- src/server/router/routes.ts
+++ src/server/router/routes.ts
@@ -7,31 +7,31 @@ export function configRoutes(server: Express) {
 		});
 	});
 
-	server.post('post/comment/:id', (req: Request, res: Response) => {
+	server.post('/post/comment/:id', (req: Request, res: Response) => {
 		res.send({
 			message: `comment post id: ${req.params.id}`,
 		});
 	});
 
-	server.post('comment/reply/:id', (req: Request, res: Response) => {
+	server.post('/comment/reply/:id', (req: Request, res: Response) => {
 		res.send({
 			message: `reply comment id: ${req.params.id}`,
 		});
 	});
 
-	server.patch('post/upvote/:id', (req: Request, res: Response) => {
+	server.patch('/post/upvote/:id', (req: Request, res: Response) => {
 		res.send({
 			message: `upvote post id: ${req.params.id}`,
 		});
 	});
 
-	server.patch('comment/upvote/:id', (req: Request, res: Response) => {
+	server.patch('/comment/upvote/:id', (req: Request, res: Response) => {
 		res.send({
 			message: `upvote comment id: ${req.params.id}`,
 		});
 	});
 
-	server.patch('reply/upvote/:id', (req: Request, res: Response) => {
+	server.patch('/reply/upvote/:id', (req: Request, res: Response) => {
 		res.send({
 			message: `upvote reply id: ${req.params.id}`,
 		});
